f740f16ec424d7f69dbc7fe58d423cf727ba09c3
Abhishek <abhimore74@gmail.com>
2022-09-01 18:06:59 -0500

    first draft

---
+++ components/LinksDesktop.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function LinksDesktop() {
+  return (
+    <div className="hidden lg:flex justify-between w-[65%] lg:w-[75%] xl:w-[65%] m-auto pt-20">
+        <a className="text-3xl font-bold blend" href="https://firebasestorage.googleapis.com/v0/b/asciify-a4fba.appspot.com/o/Abhishek%20More%20Resume.pdf?alt=media&token=84cf5ee1-8480-4338-a511-bb9be33bbeed" target="_blank" rel="noopener noreferrer">Resume &rarr;</a>
+        <a className="text-3xl font-bold blend" href="https://github.com/Abhishek-More" target="_blank" rel="noopener noreferrer">Github &rarr;</a>
+        <a className="text-3xl font-bold blend" href="https://www.linkedin.com/in/abhishek-more-linked" target="_blank" rel="noopener noreferrer">LinkedIn &rarr;</a>
+        <a className="text-3xl font-bold blend" href="https://devpost.com/AbhishekMore" target="_blank" rel="noopener noreferrer">Devpost &rarr;</a>
+    </div>
+  )
+}
